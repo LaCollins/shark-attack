b74bdf6a8b129d187a129fe4d65856245947b922
Laura Collins <laura.e.collins1@gmail.com>
2019-12-13 19:51:45 -0600

    students printing

--- src/components/Graveyard/Graveyard.js
+++ src/components/Graveyard/Graveyard.js
@@ -5,7 +5,7 @@ class Graveyard extends React.Component {
   render() {
     return (
       <div className="graveyard row d-flex justify-content-center col-5">
-        <h4 id="graveLabel">Graveyard</h4>
+        <h3 id="graveLabel">Graveyard</h3>
       </div>
     );
   }
--- src/components/SharkTank/SharkTank.js
+++ src/components/SharkTank/SharkTank.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import LiveStudent from '../LiveStudent/LiveStudent';
 import './SharkTank.scss';
+import studentShape from '../../helpers/propz/studentShape';
 
 class SharkTank extends React.Component {
+  static propTypes = {
+    students: PropTypes.arrayOf(studentShape.studentShape),
+  }
+
   render() {
+    const { students } = this.props;
+
+    const studentCards = students.map((student) => <LiveStudent key={student.id} student={student} />);
+
     return (
       <div className="sharkTank row d-flex justify-content-center col-5">
-        <h4 id="tankLabel">Shark Tank</h4>
+        <div className="row d-flex justify-content-center studentContainer">
+            {studentCards}
+        </div>
+        <h3 id="tankLabel">Shark Tank</h3>
       </div>
     );
   }
